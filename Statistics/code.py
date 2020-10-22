c8f6dbfca8934c86e077f67e13498a93c2f8391e
Ravi Shankar <67362882+ravigits@users.noreply.github.com>
2020-10-22 23:13:17 +0530

    Code file created.

---
+++ Statistics/code.py
@@ -0,0 +1,21 @@
+# --------------
+#Header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Reading of the file
+data=pd.read_csv(path)
+
+# Code starts here
+data['Gender'] = data['Gender'].replace('-','')
+
+data['Alignment'].value_counts().plot.pie()
+
+qtl = data['Total'].quantile(0.99)
+
+super_best_names  = list(data[data['Total'] > qtl]['Name'])
+print(super_best_names )
+
+
